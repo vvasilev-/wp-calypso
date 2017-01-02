2732bec668974ba4f1c0959c9a454b2691b65e31
Timmy Crawford <timmydcrawford@gmail.com>
2017-01-02 14:42:04 -0800

    Stats: add sparkline to sidebar menu.

--- client/my-sites/sidebar/sidebar.jsx
+++ client/my-sites/sidebar/sidebar.jsx
@@ -148,6 +148,15 @@ export class MySitesSidebar extends Component {
 
 		const siteId = this.isSingle() ? site.slug : null;
 
+		let sparkline;
+		if ( ! site.jetpack && ! this.isItemLinkSelected( 'stats' ) ) {
+			sparkline = (
+				<img
+					className="sidebar__sparkline"
+					src={ `https://${ site.wpcom_url }/wp-includes/charts/admin-bar-hours-scale-2x.php?masterbar=1` } />
+			);
+		}
+
 		return (
 			<SidebarItem
 				tipTarget="menus"
@@ -155,7 +164,9 @@ export class MySitesSidebar extends Component {
 				className={ this.itemLinkClass( '/stats', 'stats' ) }
 				link={ getStatsPathForTab( 'day', siteId ) }
 				onNavigate={ this.onNavigate }
-				icon="stats-alt" />
+				icon="stats-alt">
+				{ sparkline }
+			</SidebarItem>
 		);
 	}
 
