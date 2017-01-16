f7870154768aaee875791ca791e2df340331685c
Marin Atanasov <tyxla@abv.bg>
2017-01-16 17:34:59 +0200

    Jetpack Settings: Integrate Publishing Tools card into Writing settings

--- client/my-sites/site-settings/form-writing.jsx
+++ client/my-sites/site-settings/form-writing.jsx
@@ -22,10 +22,16 @@ import Card from 'components/card';
 import Button from 'components/button';
 import QueryTaxonomies from 'components/data/query-taxonomies';
 import TaxonomyCard from './taxonomies/taxonomy-card';
-import { isJetpackModuleActive, isJetpackMinimumVersion } from 'state/sites/selectors';
+import {
+	isJetpackModuleActive,
+	isJetpackMinimumVersion,
+	isJetpackSite,
+	siteSupportsJetpackSettingsUI
+} from 'state/sites/selectors';
 import { getSelectedSiteId } from 'state/ui/selectors';
 import { requestPostTypes } from 'state/post-types/actions';
 import CustomPostTypeFieldset from './custom-post-types-fieldset';
+import PublishingTools from './publishing-tools';
 
 const SiteSettingsFormWriting = React.createClass( {
 	mixins: [ dirtyLinkedState, formBase ],
@@ -204,6 +210,16 @@ const SiteSettingsFormWriting = React.createClass( {
 					</div>
 				) }
 
+				{
+					this.props.isJetpackSite && this.props.jetpackSettingsUISupported && (
+						<PublishingTools
+							submittingForm={ this.state.submittingForm }
+							onSubmitForm={ this.handleSubmitForm }
+							fetchingSettings={ this.state.fetchingSettings }
+							/>
+					)
+				}
+
 				{ config.isEnabled( 'press-this' ) && (
 					<div>
 						{
@@ -226,6 +242,8 @@ export default connect(
 		return {
 			jetpackCustomTypesModuleActive: false !== isJetpackModuleActive( state, siteId, 'custom-content-types' ),
 			jetpackVersionSupportsCustomTypes: false !== isJetpackMinimumVersion( state, siteId, '4.2.0' ),
+			jetpackSettingsUISupported: siteSupportsJetpackSettingsUI( state, siteId ),
+			isJetpackSite: isJetpackSite( state, siteId ),
 			siteId
 		};
 	},
